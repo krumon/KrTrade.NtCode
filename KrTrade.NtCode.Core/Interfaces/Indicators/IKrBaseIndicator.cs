0cf1b9a3e3a029624ce0efa7920ba3c2e5172b24
Krumon <enriqueruedamontero@gmail.com>
2023-06-20 01:10:11 +0200

    Create Nt.Service project.

---
+++ KrTrade.NtCode.Core/Interfaces/Indicators/IKrBaseIndicator.cs
@@ -0,0 +1,16 @@
+﻿namespace KrTrade.Nt.Core.Interfaces
+{
+    public interface IKrBaseIndicator
+    {
+        INinjaScriptService Ninjascript { get; }
+        void SetDefaults();
+        void Configure();
+        void DataLoaded();
+        void AddPlots();
+        void AddDataSeries();
+        void Dispose();
+        void OnBarClosed();
+        void OnEachTick();
+        void OnPriceChanged();
+    }
+}
